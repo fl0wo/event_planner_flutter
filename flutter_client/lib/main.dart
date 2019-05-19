f72226c73272e3e7595d518a94ca3eea44fd26f7
fl0wo <sabaniflorian@gmail.com>
2019-05-19 16:50:17 +0200

    added project

---
+++ flutter_client/lib/main.dart
@@ -0,0 +1,130 @@
+import 'package:circular_bottom_navigation/tab_item.dart';
+import 'package:flutter/material.dart';
+import 'package:circular_bottom_navigation/circular_bottom_navigation.dart';
+
+import 'widgets/timeline.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Circular Bottom Navigation Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'circular_bottom_navigation'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+final Color c_main =
+    Colors.deepPurpleAccent; //= Color.fromARGB(255, 83, 82, 237);
+
+class _MyHomePageState extends State<MyHomePage> {
+  int selectedPos = 1;
+
+  double bottomNavBarHeight = 45;
+
+  List<TabItem> tabItems = List.of([
+    new TabItem(Icons.account_circle, "Me", c_main),
+    new TabItem(Icons.home, "Home", c_main),
+    new TabItem(Icons.layers, "My Events", c_main),
+    new TabItem(Icons.notifications, "Notifications", c_main),
+  ]);
+
+  CircularBottomNavigationController _navigationController;
+
+  @override
+  void initState() {
+    super.initState();
+    _navigationController = new CircularBottomNavigationController(selectedPos);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          Padding(
+            child: bodyContainer(),
+            padding: EdgeInsets.only(bottom: bottomNavBarHeight),
+          ),
+          Align(alignment: Alignment.bottomCenter, child: bottomNav())
+        ],
+      ),
+    );
+  }
+
+  Widget bodyContainer() {
+    Color selectedColor = tabItems[selectedPos].color;
+    String slogan;
+    switch (selectedPos) {
+      case 0:
+        slogan = "Me";
+        break;
+      case 1:
+        return Scaffold(body: WeekView());
+        break;
+      case 2:
+        slogan = "My Events";
+        break;
+      case 3:
+        slogan = "Notifiche";
+        break;
+    }
+
+    return GestureDetector(
+      child: Container(
+        width: double.infinity,
+        height: double.infinity,
+        color: selectedColor,
+        child: Center(
+          child: Text(
+            slogan,
+            style: TextStyle(
+                color: Colors.white, fontWeight: FontWeight.bold, fontSize: 20),
+          ),
+        ),
+      ),
+      onTap: () {
+        if (_navigationController.value == tabItems.length - 1) {
+          _navigationController.value = 0;
+        } else {
+          _navigationController.value++;
+        }
+      },
+    );
+  }
+
+  Widget bottomNav() {
+    return CircularBottomNavigation(
+      tabItems,
+      controller: _navigationController,
+      barHeight: bottomNavBarHeight,
+      barBackgroundColor: Colors.white,
+      animationDuration: Duration(milliseconds: 300),
+      selectedCallback: (int selectedPos) {
+        setState(() {
+          this.selectedPos = selectedPos;
+          print(_navigationController.value);
+        });
+      },
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _navigationController.dispose();
+  }
+}
---
+++ flutter_client/lib/widgets/timeline.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+Random random = new Random();
+
+const distanza_top = 250.0;
+
+const width_block = 100.0;
+const height_block = 50.0;
+
+class WeekView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return CustomScrollView(
+      slivers: <Widget>[
+        SliverPersistentHeader(
+          delegate: WeekViewHeaderDelegate(),
+          pinned: true,
+        ),
+        SliverToBoxAdapter(
+          child: _buildGrid(),
+        )
+      ],
+      scrollDirection: Axis.horizontal,
+    );
+  }
+
+  Widget _buildGrid() {
+    return SizedBox(
+      width: width_block * 100,
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: List.generate(50, (d) => _buildColumn(d)),
+      ),
+    );
+  }
+
+  int l_appoggio;
+
+  Widget _buildColumn(int d) {
+    return Expanded(
+      child: Stack(
+        children: List.generate(l_appoggio = (1 + random.nextInt(4)),
+            (d) => _buildRect(d, l_appoggio)),
+      ),
+    );
+  }
+
+  Widget _buildRect(int i, int l) {
+    return Positioned(
+      left: 25.0 + random.nextInt(50),
+      width: width_block + random.nextInt(50) - random.nextInt(50),
+      top: distanza_top +
+          (height_block * i) -
+          (height_block * l / 2.000), //+ height_block * d,
+      height: height_block,
+      child: Container(
+        margin: EdgeInsets.symmetric(vertical: 2.0),
+        color: Colors.deepPurpleAccent,
+      ),
+    );
+  }
+}
+
+class WeekViewHeaderDelegate extends SliverPersistentHeaderDelegate {
+  @override
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return Container(
+      child: Center(
+        child: Text(''),
+      ),
+    );
+  }
+
+  @override
+  double get maxExtent => distanza_top;
+
+  @override
+  double get minExtent => distanza_top;
+
+  @override
+  bool shouldRebuild(SliverPersistentHeaderDelegate oldDelegate) {
+    return false;
+  }
+}
